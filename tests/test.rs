a382b8ce210d7337aa415ed30aadfdeac955694a
kenoss <keno.ss57@gmail.com>
2024-09-04 20:38:01 +0900

    Prepare for rewrite

--- tests/test.rs
+++ tests/test.rs
@@ -3,9 +3,4 @@ fn ui_test() {
     let t = trybuild::TestCases::new();
     t.pass("tests/ui/pass_*.rs");
     t.compile_fail("tests/ui/fail*.rs");
-    #[cfg(feature = "unstable_delegate_to")]
-    {
-        t.pass("tests/ui/delegate_to_pass_*.rs");
-        t.compile_fail("tests/ui/delegate_to_fail*.rs");
-    }
 }
